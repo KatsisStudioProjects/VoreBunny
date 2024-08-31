4be7ab6dfa1dc9b708559d0f07f63e327f3c78b7
Xwilarg <xwilarg@protonmail.com>
2024-08-31 23:45:16 +0200

    Add buttons

--- Assets/Scripts/BGMManager.cs
+++ Assets/Scripts/BGMManager.cs
@@ -1,12 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace VoreBunny
 {
     public class BGMManager : MonoBehaviour
     {
+        public static bool UseSFX = true;
+
+        [SerializeField]
+        private Image _btnSfx, _btnBgm;
+
+        [SerializeField]
+        private Sprite _sfxOn, _sfxOff, _bgmOn, _bgmOff;
+
+        private AudioSource _bgm;
+
         private void Awake()
         {
+            _bgm = GetComponent<AudioSource>();
             DontDestroyOnLoad(gameObject);
+        }
+
+        public void ToggleSfx()
+        {
+            UseSFX = !UseSFX;
+            _btnSfx.sprite = UseSFX ? _sfxOn : _sfxOff;
+        }
+
+        public void ToggleBgm()
+        {
+            _bgm.volume = _bgm.volume == 0 ? 1f : 0f;
+            _btnBgm.sprite = _bgm.volume == 1f ? _bgmOn : _bgmOff;
         }
     }
 }
